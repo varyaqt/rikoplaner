6af2d3ebf1f047afed0dea770ad167229e6cf118
varyaqt <popadinavar@ya.ru>
2024-10-11 22:24:01 +0300

    прописала класс User, проверку пароля и логина на корректность

--- main_app.py
+++ main_app.py
@@ -1,18 +1,16 @@
-from fastapi import FastAPI
-import re
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, field_validator
 
-app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "hello, milena!"}
+app = FastAPI()
 
 
 class User(BaseModel):
     username: str
     password: str
     secret_word: str
+
+
     @field_validator("username")
     @classmethod
     def validate_username(cls, value):
@@ -21,16 +19,16 @@ class User(BaseModel):
         flag_s = 1
         for c in value:
             if c not in list_symbols:
-                flag = 0
+                flag_s = 0
                 break
-        if (flag_s==0):
+        if flag_s==0:
             raise ValueError("В логине можно использовать только латиницу, цифры и символ нижнего подчеркивания")
         flag_l = 0
         for c in value:
             if c in list_letters:
                 flag_l = 1
                 break
-        if (flag_l==0):
+        if flag_l==0:
             raise ValueError("В логине должна быть хотя бы одна буква")
         return value
 
@@ -58,28 +56,9 @@ class User(BaseModel):
             raise ValueError("Пароль должен содержать хотя бы одну цифру")
         if flag_l==0:
             raise ValueError("Пароль должен содержать хотя бы одну букву")
+        return value
 
-
-
-
-class User(BaseModel):
-    username: str
-    age: int
-    email: str
-    password: str
-
-
-
-# Valid User
-valid_user = {"username": "test_name", "age": 20, "email": "NAME@test.gr", "password": "123456789"}
-user = User.model_validate(valid_user)
-
-# Invalid User
-# invalid_user = {
-#     'username': 'test_name',
-#     'age': 16,
-#     'email': 'name_test.gr',
-#     'password': '1234'
-# }
-# user = User.model_validate(invalid_user)
-user
+@app.post("/register")
+def register_user(user: User):
+    # здесь добавить логику для сохранения пользователя в базу данных
+    return {"message": "User registered successfully", "user": user}
