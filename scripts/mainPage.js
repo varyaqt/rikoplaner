0dd97997270b8040f01221887c79f09528bf14f1
Анастасия Бабенко <babenko.ase@dvfu.ru>
2024-12-02 00:02:23 +0300

    добавлено отображение сегодняшней даты в поле даты

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -2,3 +2,12 @@ new AirDatepicker('#airdatepicker', {
   inline: true
 });
 
+const today = new Date();
+const day = today.getDate();
+const month = today.getMonth() + 1; 
+const year = today.getFullYear();
+const formattedDate = `${day}.${month}.${year}`;
+
+const calendarInputElement = document.getElementById('airdatepicker');
+calendarInputElement.value = `${formattedDate}`;
+
