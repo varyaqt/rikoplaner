4f582fdada479399660607c677c95c45c29a1c5a
Анастасия Бабенко <babenko.ase@dvfu.ru>
2024-12-01 18:50:02 +0300

    проверка паролей на совпадение и формирование данных для отправки в бд

--- scripts/register-form.js
+++ scripts/register-form.js
@@ -1,7 +1,10 @@
-// проверка на то, что в поле "подтверждения пароля" введен такой же пароль
+
 const registrationFormElement = document.getElementById('registrationForm')
-registrationFormElement.addEventListener('submit', event => 
+registrationFormElement.addEventListener('submit', async (event) => 
   {
+    event.preventDefault(); // Предотвращаем отправку формы
+
+    // проверка на то, что в поле "подтверждения пароля" введен такой же пароль
     let password = document.getElementsByName('password')[0].value;
     let confirmPassword = document.getElementsByName('confirm_password')[0].value;
 
@@ -9,12 +12,31 @@ registrationFormElement.addEventListener('submit', event =>
       alert("Пароли не совпадают!");
       event.preventDefault(); // Предотвращаем отправку формы
     }
+
+    // Собираем данные с формы
+    const formData = new FormData(event.target);
+    console.log(Array.from(formData.entries()))
+
+    try {
+      const response = await fetch('/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      if (response.ok) {
+        alert('Аккаунт успешно создан!');
+        window.location.href = 'login.html'; // Перенаправление на страницу входа
+      } else {
+        const errorData = await response.json();
+        alert(`Ошибка: ${errorData.detail}`);
+      }
+    } catch (error) {
+      console.error('Ошибка при отправке данных:', error);
+      alert('Произошла ошибка при отправке данных. Пожалуйста, попробуйте снова.');
+    }
 });
 
-function handleFormSubmit(event) {
-  // Просим форму не отправлять данные самостоятельно
-  event.preventDefault()
-  console.log('Отправка!')
-}
 
-registrationFormElement.addEventListener('submit', handleFormSubmit)
