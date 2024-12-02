c38555e18c264cac62af535098f0e505db12ae02
Анастасия Бабенко <babenko.ase@dvfu.ru>
2024-12-02 23:24:48 +0300

    реализовано добавление задачи в стек

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -1,3 +1,10 @@
+import { 
+  stackList, 
+  checkFullStack, 
+  sortStackList 
+} from "../data/stackList.js";
+
+
 // calendar
 new AirDatepicker('#airdatepicker', {
   inline: true
@@ -14,52 +21,111 @@ const calendarInputElement = document.getElementById('airdatepicker');
 calendarInputElement.value = `${formattedDate}`;
 
 
-// скрываем плюсик, когда пользователь вводит новою задачу
 const inputElement = document.getElementById('inputToStack');
-const placeholderImage = document.querySelector('.add-stack-icon');
-const inputContainer = document.querySelector('.add-todo-to-stack');
+const placeholderImage = document.querySelector('.js-add-stack-icon');
+const inputContainer = document.querySelector('.js-add-todo-to-stack');
 const addToStackButtonElement = document.getElementById('addStackButton');
+const taskInStackElement = document.querySelector('.js-todo-container-stack');
+
+checkInputStackAvailable();
+
+updateSizeStack();
+
+renderStackList();
+
+// определяю размер стека
+function updateSizeStack() {
+  const sizeStackElement = document.querySelector('.js-size-stack');
+  sizeStackElement.innerHTML = `${stackList.length}/10`;
+};
+
+// проверяем заполненность стэка
+function checkInputStackAvailable() {
+  const sizeStackElement = document.querySelector('.js-size-stack');
+  if (checkFullStack() === true) {
+    inputElement.classList.remove('input-active');
+    inputElement.classList.add('not-active-input-stack');
+    sizeStackElement.style.color = 'rgb(122, 25, 25)';
+    inputElement.placeholder = 'Стек переполнен';
+    inputElement.disabled = true;
+    placeholderImage.style.display = 'none';
+  } else {
+    sizeStackElement.style.color = 'rgb(60,91,120)';
+    inputElement.classList.remove('not-active-input-stack');
+    inputElement.placeholder = 'Добавить в стек';
+    inputElement.disabled = false;
+  };
+};
+
+// при запуске загружаем весь стек на страницу
+function renderStackList() {
+  const stackListElement = document.querySelector('.js-stack-list-container');
+  sortStackList();
+  let html = ``;
+  stackList.forEach((element) => {
+    html += `
+      <div class="todo-container-stack" id="stackTask${element.id}">
+        ${element.name} 
+      </div>
+    `;
+  });
+  stackListElement.innerHTML = html;
+};
+
+// добавление новой задачи в стек
+function addTaskToStack(taskName) {
+  let taskId = 0;
+  if (stackList.length !== 0) {
+    taskId = Number(stackList[0].id) + 1;
+  } else if (stackList.length === 0) {
+    taskId = 1;
+  }
+  const newTask = {
+    id: taskId,
+    name: taskName
+  };
+  stackList.push(newTask);
+  renderStackList();
+  updateSizeStack();
+  inputElement.value = '';
+  checkInputStackAvailable();
+}
 
 
-// Скрываем изображение при фокусе на поле ввода
-inputElement.addEventListener('input', () => {
-  placeholderImage.style.display = 'none';
-  if (inputElement.value.trim() !== '') {
-    // Убираем левый padding, если поле ввода не пустое
-    inputElement.style.paddingLeft = '8px';
-    inputElement.style.borderTopRightRadius = 0;
-    inputElement.style.borderBottomRightRadius = 0;
-    addToStackButtonElement.style.display = 'block';
-    inputElement.classList.add('input-active');
-  } 
+
+// Скрываем плюсик при фокусе на поле ввода
+inputElement.addEventListener('focus', () => {
+  placeholderImage.classList.add('unvisible-input-img');
+  inputElement.classList.add('input-active');
+  addToStackButtonElement.style.display = 'block';
+  
 });
 
 
-  // Показываем изображение, если поле ввода пустое и не в фокусе
-  inputElement.addEventListener('blur', () => {
-    if (inputElement.value === '') {
-      placeholderImage.style.display = 'block';
-      inputElement.style.paddingLeft = '95px';
-      addToStackButtonElement.style.display = 'none';
-      inputElement.style.borderTopRightRadius = '30px';
-      inputElement.style.borderBottomRightRadius = '30px';
-      inputElement.classList.remove('input-active');
+// Показываем изображение, если поле ввода пустое и не в фокусе
+inputElement.addEventListener('blur', () => {
+  if (inputElement.value === '') {
+    placeholderImage.classList.remove('unvisible-input-img');
+    inputElement.classList.remove('input-active');
+    addToStackButtonElement.style.display = 'none';
+  }
+});
+
+// добавление задачи в стэк при нажатии кнопки или enter
+addToStackButtonElement.addEventListener('click', () => {
+  const newTask = inputElement.value;
+  if (newTask !== '') {
+    addTaskToStack(newTask);
+  }
+});
+
+inputElement.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    const newTask = inputElement.value;
+    if (newTask !== '') {
+      addTaskToStack(newTask);
     }
-  });
-
-function addTaskToStack() {
-  
-};
-
-  // добавление задачи в стэк при нажатии кнопки или enter
-  addToStackButtonElement.addEventListener('click', () => {
-    addTaskToStack();
-  });
-
-  addToStackButtonElement.addEventListener('keydown', (event) => {
-    if (event.key == 'Enter') {
-      addTaskToStack();
-    }
-  });
+  }
+});
 
 
