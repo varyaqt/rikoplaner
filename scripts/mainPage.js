988604c9acc51bb6f0c63be822576c494b56681b
Анастасия Бабенко <babenko.ase@dvfu.ru>
2024-12-02 21:13:47 +0300

    частично реализована работа поля ввода задачи в стек

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -1,7 +1,9 @@
+// calendar
 new AirDatepicker('#airdatepicker', {
   inline: true
 });
 
+// make a default value of calendar input as a today's date
 const today = new Date();
 const day = today.getDate();
 const month = today.getMonth() + 1; 
@@ -10,4 +12,54 @@ const formattedDate = `${day}.${month}.${year}`;
 
 const calendarInputElement = document.getElementById('airdatepicker');
 calendarInputElement.value = `${formattedDate}`;
+
+
+// скрываем плюсик, когда пользователь вводит новою задачу
+const inputElement = document.getElementById('inputToStack');
+const placeholderImage = document.querySelector('.add-stack-icon');
+const inputContainer = document.querySelector('.add-todo-to-stack');
+const addToStackButtonElement = document.getElementById('addStackButton');
+
+
+// Скрываем изображение при фокусе на поле ввода
+inputElement.addEventListener('input', () => {
+  placeholderImage.style.display = 'none';
+  if (inputElement.value.trim() !== '') {
+    // Убираем левый padding, если поле ввода не пустое
+    inputElement.style.paddingLeft = '8px';
+    inputElement.style.borderTopRightRadius = 0;
+    inputElement.style.borderBottomRightRadius = 0;
+    addToStackButtonElement.style.display = 'block';
+    inputElement.classList.add('input-active');
+  } 
+});
+
+
+  // Показываем изображение, если поле ввода пустое и не в фокусе
+  inputElement.addEventListener('blur', () => {
+    if (inputElement.value === '') {
+      placeholderImage.style.display = 'block';
+      inputElement.style.paddingLeft = '95px';
+      addToStackButtonElement.style.display = 'none';
+      inputElement.style.borderTopRightRadius = '30px';
+      inputElement.style.borderBottomRightRadius = '30px';
+      inputElement.classList.remove('input-active');
+    }
+  });
+
+function addTaskToStack() {
+  
+};
+
+  // добавление задачи в стэк при нажатии кнопки или enter
+  addToStackButtonElement.addEventListener('click', () => {
+    addTaskToStack();
+  });
+
+  addToStackButtonElement.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter') {
+      addTaskToStack();
+    }
+  });
+
 
